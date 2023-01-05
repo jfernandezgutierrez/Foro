2f34baf8935883b32ff9b00f0f57190005fa5a25
jfernandezgutierrez <jfernandegutierrez@gmail.com>
2023-01-05 15:30:11 +0100

    Primer comit

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <span class="navbar-brand">Simulador de Tweets</span>
+
+      <button
+        class="btn pl-4 pr-4 btn-success"
+        @click="openCloseForm"
+        :class="{ 'btn-success': !showForm, 'btn-danger': showForm }"
+      >
+        {{ !showForm ? "Nuevo tweet" : "Cerrar Tweet" }}
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    showForm: Boolean,
+    openCloseForm: Function,
+  },
+};
+</script>
+
+<style></style>
